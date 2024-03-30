f7880c4fcae6ce5ca7d63a64a1e7ac1aaae6819d
IgorMontezuma20 <igorminranda17@gmail.com>
2024-03-30 15:49:47 -0300

    feat: Create Login Service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,8 +5,10 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -16,13 +18,14 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
     PrimaryInputComponent,
     ReactiveFormsModule,
   ],
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(private router: Router, private loginService: LoginService) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
@@ -30,5 +33,21 @@ export class LoginComponent {
         Validators.minLength(6),
       ]),
     });
+  }
+
+  submit() {
+    const { email, password } = this.loginForm.value;
+    this.loginService.login(email, password).subscribe({
+      next: () => {
+        console.log('Success');
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  navigate() {
+    this.router.navigate(['/signup']);
   }
 }
